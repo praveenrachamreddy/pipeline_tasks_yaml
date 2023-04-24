95bb1d61a519b22ec2c76e962891ec803839ee92
Sumit Kumar Biswal <sumitbiswal98@gmail.com>
2023-04-24 18:02:17 +0530

    Create pipeline_task.sh

---
+++ sh_files/pipeline_task.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Define variables for pipeline and task configurations
+PIPELINE_CONFIG="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/pipeline/pipeline.yaml"
+TASK1_GIT_KUSTOMIZE="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/tasks/git-kustomize.yaml"
+TASK2_SEND_MAIL="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/tasks/logs.yaml"
+TASK2_BUILDAH="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/tasks/buildah.yaml"
+
+# Create pipeline and tasks
+oc apply -f $PIPELINE_CONFIG
+oc apply -f $TASK1_GIT_KUSTOMIZE
+oc apply -f $TASK2_SEND_MAIL
+#oc apply -f $TASK2_BUILDAH
+
+# Set the project/namespace where the PVC will be created
+PROJECT=sumit
+
+# Set the name and size of the PVC
+PVC_NAME=source
+PVC_SIZE=1Gi
+
+# Create the PVC
+oc create -f - <<EOF
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: $PVC_NAME
+spec:
+  accessModes:
+    - ReadWriteOnce
+  resources:
+    requests:
+      storage: $PVC_SIZE
+EOF
+
+
+
+#pvc
+oc create -f https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/pvc/mail_pvc.yaml
+
+#secret
+kubectl apply -f https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/secret/secret-mail.yaml 
+
+#pvc
+kubectl apply -f https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/pvc/pvc.yaml
