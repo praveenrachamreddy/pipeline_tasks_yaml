d2e9b6d5d9efd37d547ff946f77e268086df15c1
Sumit Kumar Biswal <sumitbiswal98@gmail.com>
2023-05-02 20:32:03 +0530

    Update pipeline_task.sh

--- sh_files/pipeline_task.sh
+++ sh_files/pipeline_task.sh
@@ -5,26 +5,22 @@ TASK1_GIT_KUSTOMIZE="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/ma
 TASK2_SEND_MAIL="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/tasks/logs.yaml"
 TASK2_BUILDAH="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/tasks/buildah.yaml"
 TASK3_APPROVAL="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/approval_task_code/approval_task.yaml"
+FRONTEND_CONFIGMAP="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/approval_task_code/frontend_cm.yaml"
+BACKEND_CONFIGMAP="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/approval_task_code/frontend_cm.yaml"
+NEXUS_TEMPLATE="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/Templates/nexus.yaml"
+SONARQUBE_TEMPLATE="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/Templates/sonarqube.yaml"
+PVC="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/pvc/pvc.yaml"
+SECRET="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/secret/server-secret.yaml"
+
 
 # Create pipeline and tasks
 oc apply -f $PIPELINE_CONFIG
 oc apply -f $TASK1_GIT_KUSTOMIZE
 oc apply -f $TASK2_SEND_MAIL
-#oc apply -f $TASK2_BUILDAH
-oc apply -f $TASK_APPROVAL
-
-
-# Set the name and size of the PVC
-
-#secret
-kubectl apply -f https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/secret/server-secret.yaml 
-
-#pvc
-kubectl apply -f https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/pvc/pvc.yaml
-
-#Templates-nexus
-oc apply -f https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/Templates/nexus.yaml
-
-#Templates-sonarqube
-oc apply -f https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/Templates/sonarqube.yaml
-
+oc apply -f $TASK3_APPROVAL
+oc apply -f $FRONTEND_CONFIGMAP
+oc apply -f $BACKEND_CONFIGMAP
+oc apply -f $NEXUS_TEMPLATE
+oc apply -f $SONARQUBE_TEMPLATE
+oc apply -f $PVC
+oc apply -f $SECRET
