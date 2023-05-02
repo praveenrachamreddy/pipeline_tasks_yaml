033b45b465b57d589e93c3d5fb3c49ba80a4e2da
Sumit Kumar Biswal <sumitbiswal98@gmail.com>
2023-05-02 20:06:52 +0530

    Update pipeline_task.sh

--- sh_files/pipeline_task.sh
+++ sh_files/pipeline_task.sh
@@ -4,38 +4,17 @@ PIPELINE_CONFIG="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/p
 TASK1_GIT_KUSTOMIZE="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/tasks/git-kustomize.yaml"
 TASK2_SEND_MAIL="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/tasks/logs.yaml"
 TASK2_BUILDAH="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/tasks/buildah.yaml"
+TASK3_APPROVAL="https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/approval_task_code/approval_task.yaml"
 
 # Create pipeline and tasks
 oc apply -f $PIPELINE_CONFIG
 oc apply -f $TASK1_GIT_KUSTOMIZE
 oc apply -f $TASK2_SEND_MAIL
 #oc apply -f $TASK2_BUILDAH
+oc apply -f $TASK_APPROVAL
 
-# Set the project/namespace where the PVC will be created
-PROJECT=sumit
 
 # Set the name and size of the PVC
-PVC_NAME=source
-PVC_SIZE=1Gi
-
-# Create the PVC
-oc create -f - <<EOF
-apiVersion: v1
-kind: PersistentVolumeClaim
-metadata:
-  name: $PVC_NAME
-spec:
-  accessModes:
-    - ReadWriteOnce
-  resources:
-    requests:
-      storage: $PVC_SIZE
-EOF
-
-
-
-#pvc
-oc create -f https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/pvc/mail_pvc.yaml
 
 #secret
 kubectl apply -f https://github.com/sumitbiswal98/pipeline_tasks_yaml/raw/main/secret/secret-mail.yaml 
